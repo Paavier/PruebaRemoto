15f62f848b01a1877632b0e6628a3ddf476f5224
Paavier <pedrojavierof29@gmail.com>
2025-04-04 18:28:31 +0200

    new Class Employee

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,4 +1,20 @@
 package org.example;
 
 public class Employee {
+
+     private String id;
+     private String name;
+     private double baseSalary;
+     private int yearsWorked;
+     private String department;
+     private boolean isFullTime;
+
+     public Employee (String id, String name, double baseSalary, int yearsWorked, String department, boolean isFullTime){
+          this.id = id;
+          this.name = name;
+          this.baseSalary = baseSalary;
+          this.yearsWorked = yearsWorked;
+          this.department = department;
+          this.isFullTime = isFullTime;
+     }
 }
