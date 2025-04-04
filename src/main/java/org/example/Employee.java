faa7e93b3becc8a57c748f0b698564d4cee10b98
Paavier <pedrojavierof29@gmail.com>
2025-04-04 18:48:03 +0200

    added setter and getters methods

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Employee {
 
-     private String id;
+     private final String id;
      private String name;
      private final double baseSalary;
      private int yearsWorked;
@@ -18,5 +18,57 @@ public class Employee {
           this.department = department;
           this.isFullTime = isFullTime;
           this.currentSalary = baseSalary;
+     }
+
+     public String getId() {
+          return id;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public double getBaseSalary() {
+          return baseSalary;
+     }
+
+     public int getYearsWorked() {
+          return yearsWorked;
+     }
+
+     public void setYearsWorked(int yearsWorked) {
+          if (yearsWorked < 0)
+               throw new IllegalArgumentException("El tiempo trabajado no puede ser negativo");
+          this.yearsWorked = yearsWorked;
+     }
+
+     public String getDepartment() {
+          return department;
+     }
+
+     public void setDepartment(String department) {
+          this.department = department;
+     }
+
+     public boolean isFullTime() {
+          return isFullTime;
+     }
+
+     public void setFullTime(boolean fullTime) {
+          isFullTime = fullTime;
+     }
+
+     public double getCurrentSalary() {
+          return currentSalary;
+     }
+
+     public void setCurrentSalary(double currentSalary) {
+          if (currentSalary < 0)
+               throw new IllegalArgumentException("El salario no puede ser negativo");
+          this.currentSalary = currentSalary;
      }
 }
