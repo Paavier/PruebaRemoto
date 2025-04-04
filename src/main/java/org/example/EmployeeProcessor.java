5fa283f104368cec1e7df31b563c5f0ca915c2d7
Paavier <pedrojavierof29@gmail.com>
2025-04-04 19:27:47 +0200

    metodo ajustePorImpuestos añadido

--- src/main/java/org/example/EmployeeProcessor.java
+++ src/main/java/org/example/EmployeeProcessor.java
@@ -20,17 +20,9 @@ public class EmployeeProcessor {
 
                 //Ajustes por impuestos
                 if (applyTax) {
-                    if (employee.getCurrentSalary() > 50000) {
-                        newSalary = employee.getCurrentSalary() - employee.getCurrentSalary() * 0.3;
-                        employee.setCurrentSalary(newSalary);
-                    } else if (employee.getCurrentSalary() > 30000) {
-                        newSalary = employee.getCurrentSalary() - employee.getCurrentSalary() * 0.2;
-                        employee.setCurrentSalary(newSalary);
-                    } else {
-                        newSalary = employee.getCurrentSalary() - employee.getCurrentSalary() * 0.1;
-                        employee.setCurrentSalary(newSalary);
-                    }
+                    ajustePorImpuestos(employee);
                 }
+
             } else {
                 if (applyBonus && isEndOfYear) {
                     newSalary = employee.getCurrentSalary() + 200;
@@ -63,6 +55,21 @@ public class EmployeeProcessor {
             }
         } else if (employee.getDepartment().equals("HR")) {
             newSalary += 300;
+        }
+
+        employee.setCurrentSalary(newSalary);
+    }
+
+    private void ajustePorImpuestos (Employee employee){
+        double currentSalary = employee.getCurrentSalary();
+        double newSalary = employee.getCurrentSalary();
+
+        if (currentSalary > 50000) {
+            newSalary -= currentSalary * 0.3;
+        } else if (currentSalary > 30000) {
+            newSalary -= currentSalary * 0.2;
+        } else {
+            newSalary -= currentSalary * 0.1;
         }
 
         employee.setCurrentSalary(newSalary);
